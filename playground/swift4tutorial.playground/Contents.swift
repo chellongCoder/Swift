9ee76958892ba430b4b62d07d3d398fc5920b332
chellong98 <chellong98.act@gmail.com>
2018-06-19 11:35:28 +0700

    play ground

---
+++ playground/swift4tutorial.playground/Contents.swift
@@ -0,0 +1,35 @@
+
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let pi = 3.14
+
+//var x = 102.3
+
+//var name = "Hoang", age=20, email = "nhatlong@hocvienact.edu.vn"
+//
+//var x,y,z : Double
+//
+//print("name = \(name), age = \(age)")
+
+let 😥 = "i love you - 👽"
+let oneBillion = 1_000_000_000
+print("one billion> \(oneBillion)")
+
+let aNumber: UInt64 = 100
+let anotherNumber : UInt8 =  10
+let result = aNumber + UInt64(anotherNumber)
+print("result> \(result)")
+
+typealias BigNumber = Double
+let numberOfAnts:BigNumber = 1_000_000
+print("numberofants > \(numberOfAnts)")
+
+var numberOfNewMessages: Int? = 10;
+numberOfNewMessages = nil;
+print("numberofnewmessage> \(numberOfNewMessages)")
+
+
